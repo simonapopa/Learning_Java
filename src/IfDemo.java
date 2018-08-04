bab2c2423df38c5dd99f7d2637d00a3461c8c383
simona.popa <popa_simona_bh@yahoo.com>
2018-08-04 18:56:19 +0300

    first commit

---
+++ src/IfDemo.java
@@ -0,0 +1,16 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=2;
+		b=3;
+		
+		if (a<b)
+			System.out.println("a is less than b");
+		if (a==b)
+			System.out.println();
+
+	}
+
+}
