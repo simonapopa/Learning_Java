70fb8cd71c2cd408c4d92ccec0df6fae1bf1bb13
simonapopa <simonapopa@Evo215>
2018-08-14 16:56:29 +0300

    StrDemo.java

---
+++ src/StrDemo.java
@@ -0,0 +1,18 @@
+
+public class StrDemo {
+
+	public static void main(String[] args) {
+		System.out.println("First line \nSecond line");
+		System.out.println("A\tB\tC");
+		System.out.println("D\tE\tF");
+		System.out.println("Third line");
+		System.out.println("\'Single\'quote");
+		System.out.println("\"Double\"quote");
+		System.out.println("Back\\slash");
+		System.out.println("Carriage\r return");
+		System.out.println("Form\f feed");
+		System.out.println("Horizontal\t tab");
+		System.out.println("back\bspace");		
+	}
+
+}
