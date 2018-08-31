0261726d0423685de2dfbe9348869654623e1f3e
simonapopa <simonapopa@Evo215>
2018-08-31 14:50:57 +0300

    LogicalOpTable+VastDemo

---
+++ src/LogicalOpTable.java
@@ -0,0 +1,12 @@
+
+public class LogicalOpTable {
+	public static void main(String args[]) {
+		int ColOne,ColTwo,ColThree;
+		
+		//System.out.println("|Header|");
+		System.out.println("|Value1\t|Value2\t|Value3|");
+		System.out.println("|------\t|------\t|------|");
+		System.out.println("|Value4\t|Value5\t|Value6|");
+		
+	}
+}
