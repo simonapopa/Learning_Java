6964443b67c1fe8b5c61e811b6dd274c1e7d7f7c
simonapopa <simonapopa@Evo215>
2018-09-06 17:15:09 +0300

    PrimeNumbers

---
+++ src/PrimeNumber.java
@@ -0,0 +1,14 @@
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 2; i <= 10; i++) {
+			if(i%2 == 0)
+				System.out.println("Prime numbers: "+ i);
+		}
+
+	}
+
+}
