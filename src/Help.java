3afd1627b192d6f329825849af0f9b3c02057dc4
simonapopa <simonapopa@Evo215>
2018-09-14 12:57:41 +0300

    Help.java changes

--- src/Help.java
+++ src/Help.java
@@ -6,7 +6,8 @@ public class Help {
 
 		char answer;
 
-		System.out.println("Help on: \n    1. if \n    2. switch \n    3. countdown");
+		System.out.println(
+				"Help on: \n    1. if \n    2. switch \n    3. for \n    4. while \n    5. do-while \n    6. countdown");
 		System.out.print("Choose one: ");
 
 		answer = (char) System.in.read();
@@ -26,10 +27,31 @@ public class Help {
 			System.out.println("\t...");
 			System.out.println("\tdefault:\n\t\t statement sequence \n}");
 			break;
+
 		case '3':
+			System.out.println("\nThe for statement");
+			System.out.println("\nfor(initialization;condition;iteration)");
+			System.out.println("\n\tstatement;");
+			break;
+
+		case '4':
+			System.out.println("\nThe while statement");
+			System.out.println("\nwhile(condition)");
+			System.out.println("\n\tstatement;");
+			break;
+
+		case '5':
+			System.out.println("\nThe do-while statement");
+			System.out.println("\ndo{");
+			System.out.println("\n\tstatement;");
+			System.out.println("\n}while(condition);");
+			break;
+
+		case '6':
 			int x;
 			for (x = 0; x >= -5; x--)
 				System.out.println(x);
+			break;
 
 		default:
 			System.out.print("Value entered does not exist.");
